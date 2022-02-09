4dde6cf4c3bf20446adebc132e72785783878f7a
Mark Erbaugh <mark.election@gmail.com>
2022-02-08 21:03:17 -0500

    Initial Commit

---
+++ NetworkIcom/NetworkIcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkIcomApp.swift
+//  NetworkIcom
+//
+//  Created by Mark Erbaugh on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkIcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
